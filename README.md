cc9ac9fe3a0852c97acc217972d484ae211dd729
Yoshiya Hinosawa <stibium121@gmail.com>
2024-08-29 12:01:02 +0900

    chore: no semi colons

--- README.md
+++ README.md
@@ -17,24 +17,24 @@ deno add @kt3k/weak-value-map
 ### Usage
 
 ```ts
-import { WeakValueMap } from "@kt3k/weak-value-map";
-import { assert, assertFalse } from "@std/assert";
+import { WeakValueMap } from "@kt3k/weak-value-map"
+import { assert, assertFalse } from "@std/assert"
 
-const wvm = new WeakValueMap();
+const wvm = new WeakValueMap()
 
-let myObject = {/* some large object */};
+let myObject = {/* some large object */}
 
-wvm.set(1, myObject);
+wvm.set(1, myObject)
 
-assert(wvm.has(1));
-assert(wvm.get(1) === myObject);
+assert(wvm.has(1))
+assert(wvm.get(1) === myObject)
 
-myObject = null; // the object doesn't have reference anymore
+myObject = null // the object doesn't have reference anymore
 
 // ... some time later
 
-assertFalse(wvm.has(1));
-assert(wvm.get(1) === undefined);
+assertFalse(wvm.has(1))
+assert(wvm.get(1) === undefined)
 ```
 
 ## License
--- mod.ts
+++ mod.ts
@@ -2,36 +2,36 @@
  * Cache values with weak reference with values
  */
 export class WeakValueMap<K, V extends WeakKey> implements Map<K, V> {
-  readonly #map = new Map<K, WeakRef<V>>();
+  readonly #map = new Map<K, WeakRef<V>>()
   readonly #registry = new FinalizationRegistry<K>((key) => {
-    this.#map.delete(key);
+    this.#map.delete(key)
   });
-  [Symbol.toStringTag] = "WeakValueMap";
+  [Symbol.toStringTag] = "WeakValueMap"
 
   constructor(iterable: Iterable<[K, V]> = []) {
     for (const [k, v] of iterable) {
-      this.set(k, v);
+      this.set(k, v)
     }
   }
 
   clear(): void {
     for (const key of this.keys()) {
-      this.delete(key);
+      this.delete(key)
     }
   }
 
   delete(key: K): boolean {
-    const ref = this.#map.get(key);
+    const ref = this.#map.get(key)
 
     if (ref) {
-      this.#map.delete(key);
-      this.#registry.unregister(ref);
+      this.#map.delete(key)
+      this.#registry.unregister(ref)
       if (ref.deref() === undefined) {
-        return false;
+        return false
       }
-      return true;
+      return true
     }
-    return false;
+    return false
   }
 
   forEach(
@@ -40,91 +40,91 @@ export class WeakValueMap<K, V extends WeakKey> implements Map<K, V> {
     thisArg?: any,
   ): void {
     this.#map.forEach((ref, k) => {
-      callbackfn(ref.deref()!, k, thisArg);
-    });
+      callbackfn(ref.deref()!, k, thisArg)
+    })
   }
 
   get(key: K): V | undefined {
-    const ref = this.#map.get(key);
+    const ref = this.#map.get(key)
     if (ref === undefined) {
-      return undefined;
+      return undefined
     }
-    const value = ref.deref();
+    const value = ref.deref()
     if (value === undefined) {
-      this.#map.delete(key);
-      this.#registry.unregister(ref);
-      return undefined;
+      this.#map.delete(key)
+      this.#registry.unregister(ref)
+      return undefined
     }
 
-    return value;
+    return value
   }
 
   has(key: K): boolean {
-    const ref = this.#map.get(key);
+    const ref = this.#map.get(key)
     if (ref === undefined) {
-      return false;
+      return false
     }
-    const value = ref.deref();
+    const value = ref.deref()
     if (value === undefined) {
-      this.#map.delete(key);
-      this.#registry.unregister(ref);
-      return false;
+      this.#map.delete(key)
+      this.#registry.unregister(ref)
+      return false
     }
 
-    return true;
+    return true
   }
 
   set(key: K, value: V): this {
-    const prevRef = this.#map.get(key);
+    const prevRef = this.#map.get(key)
     if (prevRef) {
-      this.#registry.unregister(prevRef);
+      this.#registry.unregister(prevRef)
     }
-    const ref = new WeakRef(value);
-    this.#map.set(key, ref);
-    this.#registry.register(value, key, ref);
-    return this;
+    const ref = new WeakRef(value)
+    this.#map.set(key, ref)
+    this.#registry.register(value, key, ref)
+    return this
   }
 
   get size(): number {
     // This is not accurate, but it's the best we can do
-    return this.#map.size;
+    return this.#map.size
   }
 
   [Symbol.iterator](): IterableIterator<[K, V]> {
-    return this.entries();
+    return this.entries()
   }
 
   *entries(): IterableIterator<[K, V]> {
     for (const [k, ref] of this.#map.entries()) {
-      const v = ref.deref();
+      const v = ref.deref()
       if (v === undefined) {
-        this.#map.delete(k);
-        this.#registry.unregister(ref);
-        continue;
+        this.#map.delete(k)
+        this.#registry.unregister(ref)
+        continue
       }
-      yield [k, v];
+      yield [k, v]
     }
   }
 
   *keys(): IterableIterator<K> {
     for (const [k, ref] of this.#map.entries()) {
-      const v = ref.deref();
+      const v = ref.deref()
       if (v === undefined) {
-        this.#map.delete(k);
-        this.#registry.unregister(ref);
-        continue;
+        this.#map.delete(k)
+        this.#registry.unregister(ref)
+        continue
       }
-      yield k;
+      yield k
     }
   }
 
   *values(): IterableIterator<V> {
     for (const ref of this.#map.values()) {
-      const v = ref.deref();
+      const v = ref.deref()
       if (v === undefined) {
-        continue;
+        continue
       }
-      yield v;
+      yield v
     }
   }
 }
